7a5c096e557dfd25f9f4a064a047eaa57e419c65
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2024-02-19 18:54:29 -0300

    Updated header with more modern design

--- src/app/Components/Layout/Header/Header.tsx
+++ src/app/Components/Layout/Header/Header.tsx
@@ -1,11 +1,11 @@
 import Logo from "../../Logo/Logo"
 import { IoIosSearch } from "react-icons/io";
 
-
 export default function Header() {
 
   const navLink = "text-black text-sm font-medium hover:text-brightOrange";
-  const inputSearch = "bg-transparent border-solid border-2 border-brightOrange rounded-lg w-40 h-8 font-bold indent-2 lg:indent-7 text-xs hover:border-gray-500 focus:outline-none focus:border-blue-500 md:w-52 lg:w-80"
+  const inputSearch = "bg-transparent border-solid border-2 border-brightOrange w-40 h-8 rounded-l-lg indent-2 text-xs focus:outline-none focus:border-burntSienna md:w-52 lg:w-80"
+  const buttonSearch = "bg-brightOrange rounded-r-lg flex justify-center items-center w-10 hover:bg-burntSienna"
 
   return (
     <header className="bg-ghostWhite flex p-2 justify-around items-center lg:p-4 lg:gap-24 ">
@@ -16,10 +16,12 @@ export default function Header() {
         <a href="receitas" className={navLink}>Receitas</a>
       </nav>
 
-      <form action="" className="">
-        <input type="search" name="" id="" placeholder="Qual receita você está procurando?" className={inputSearch} />
-        <svg className="hidden lg:block absolute right-[395px] top-[24px] h-4 w-4 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/></svg>
-      </form>
+      <div className="flex">
+        <input type="search" placeholder="Procure as receitas, ingredientes ou produtos..." className={inputSearch}/>
+        <button className={buttonSearch}>
+          <IoIosSearch className="text-white"/>
+        </button>
+      </div>
     </header>
   )
 }
