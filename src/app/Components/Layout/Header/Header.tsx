0b68b41fbb91b5766509fa8f16f96bf44a09f45c
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2024-02-21 18:39:33 -0300

    Add more navigations

--- src/app/Components/Layout/Header/Header.tsx
+++ src/app/Components/Layout/Header/Header.tsx
@@ -4,16 +4,18 @@ import { IoIosSearch } from "react-icons/io";
 export default function Header() {
 
   const navLink = "text-black text-sm font-medium hover:text-brightOrange";
-  const inputSearch = "bg-transparent border-solid border-2 border-brightOrange w-40 h-8 rounded-l-lg indent-2 text-xs focus:outline-none focus:border-burntSienna md:w-52 lg:w-80"
+  const inputSearch = "bg-transparent border-solid border-2 border-brightOrange w-40 h-8 rounded-l-lg indent-2 text-xs focus:outline-none focus:border-burntSienna md:w-52 lg:w-72"
   const buttonSearch = "bg-brightOrange rounded-r-lg flex justify-center items-center w-10 hover:bg-burntSienna"
 
   return (
-    <header className="w-full h-auto bg-ghostWhite flex p-2 justify-around items-center lg:p-4 lg:gap-24">
+    <header className="w-full h-auto flex p-2 justify-between items-center lg:p-4 lg:gap-24">
       <Logo />
 
       <nav className="hidden md:flex gap-6">
         <a href="inicio" className={navLink}>Inicio</a>
         <a href="receitas" className={navLink}>Receitas</a>
+        <a href="receitas" className={navLink}>Sobre nós</a>
+        <a href="receitas" className={navLink}>Contato</a>
       </nav>
 
       <div className="flex">
