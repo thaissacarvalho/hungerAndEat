d851bc726ef1847ae3d67334ea995df571941cdf
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2024-03-14 14:24:50 -0300

    Modified file to input search

--- src/app/Components/Layout/Header/Header.tsx
+++ src/app/Components/Layout/Header/Header.tsx
@@ -1,11 +1,9 @@
-import Logo from "../../Logo/Logo"
-import { IoIosSearch } from "react-icons/io";
+import Logo from "../../Logo/Logo";
+import SearchBar from "./SearchBar/SearchBar";
 
 export default function Header() {
 
   const navLink = "text-black text-sm font-medium hover:text-brightOrange";
-  const inputSearch = "bg-transparent border-solid border-2 border-brightOrange w-40 h-8 rounded-l-lg indent-2 text-xs focus:outline-none focus:border-burntSienna md:w-52 lg:w-72"
-  const buttonSearch = "bg-brightOrange rounded-r-lg flex justify-center items-center w-10 hover:bg-burntSienna"
 
   return (
     <header className="w-full h-auto flex p-2 justify-between items-center lg:p-4 lg:gap-24">
@@ -17,13 +15,8 @@ export default function Header() {
         <a href="receitas" className={navLink}>Sobre nós</a>
         <a href="receitas" className={navLink}>Contato</a>
       </nav>
-
-      <div className="flex">
-        <input type="search" placeholder="Procure as receitas, ingredientes ou produtos..." className={inputSearch}/>
-        <button className={buttonSearch}>
-          <IoIosSearch className="text-white"/>
-        </button>
-      </div>
+      
+      <SearchBar/>
     </header>
   )
 }
